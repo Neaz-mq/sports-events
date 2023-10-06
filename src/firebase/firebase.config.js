d2eddd7fdb50ebbe54ba5464230c788c7e59bb83
Neaz-mq <neazmorshed666@gmail.com>
2023-10-06 19:59:29 +0600

    environment setup successfully

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWshC0Sitl09GactY2vcauN56pm27pxI4",
+  authDomain: "sports-events-3ca9f.firebaseapp.com",
+  projectId: "sports-events-3ca9f",
+  storageBucket: "sports-events-3ca9f.appspot.com",
+  messagingSenderId: "453866886868",
+  appId: "1:453866886868:web:f79aeeb781e0e2ecbb0d41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
